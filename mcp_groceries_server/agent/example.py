f2ef992aae83c0284648d1257e1226332e75f2b0
OBOne <orelba93@gmail.com>
2025-05-30 00:33:03 +0300

    chore(loader): add loader to agent execution (#2)

--- mcp_groceries_server/agent/example.py
+++ mcp_groceries_server/agent/example.py
@@ -2,9 +2,10 @@ import asyncio
 import logging
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
 
-from mcp_groceries_server.agent.groceries_agent import GroceriesAgent
+from mcp_groceries_server.agent.groceries_agent import GroceriesAgent  # noqa: E402
 
 
 DEBUG = os.environ.get("DEBUG", "false").lower() == "true"
@@ -12,12 +13,14 @@ logging.basicConfig(level=logging.DEBUG if DEBUG else logging.INFO)
 
 
 def main():
-    grocery_list = open("./grocery.txt", "r").read()
+    grocery_list = open("./grocery.txt", "r", encoding="utf-8").read()  # pylint: disable=consider-using-with
     if os.path.exists("./preferences.txt"):
-        preferences = open("./preferences.txt", "r").read()
+        preferences = open("./preferences.txt", "r", encoding="utf-8").read()  # pylint: disable=consider-using-with
     else:
         preferences = ""
-    coroutine = GroceriesAgent().invoke(shopping_list=grocery_list, preferences=preferences, debug=DEBUG)
+    coroutine = GroceriesAgent().invoke(
+        shopping_list=grocery_list, preferences=preferences, debug=DEBUG
+    )
     result = asyncio.run(coroutine)
     logging.info("Results:\n%s", result["messages"][-1].content)
 
--- mcp_groceries_server/agent/groceries_agent.py
+++ mcp_groceries_server/agent/groceries_agent.py
@@ -1,12 +1,12 @@
 import os
 
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_mcp_adapters.client import MultiServerMCPClient
 from langchain_mcp_adapters.tools import load_mcp_tools
 from langchain_ollama import ChatOllama
 from langgraph.prebuilt import create_react_agent
 from mcp import ClientSession, StdioServerParameters
 from mcp.client.stdio import stdio_client
+from rich.console import Console
 
 from mcp_groceries_server.agent import variables
 
@@ -20,25 +20,23 @@ def create_llm_client(model_id: str):
             timeout=None,
             max_retries=2,
         )
-    elif any([open_source in model_id for open_source in ["llama", "qwq", "deepseek"]]):
+    if any(open_source in model_id for open_source in ["llama", "qwq", "deepseek"]):
         return ChatOllama(model=model_id, temperature=0, top_k=40, top_p=0.95)
-    else:
-        raise ValueError(f"Invalid llm model {model_id}")
+
+    raise ValueError(f"Invalid llm model {model_id}")
 
 
 class GroceriesAgent:
     def __init__(self):
         self._model = create_llm_client(variables.MODEL_ID)
+        self.console = Console()
 
-    async def invoke(self, shopping_list: str, *, preferences: str = "", debug: bool = False) -> dict:
+    async def invoke(
+        self, shopping_list: str, *, preferences: str = "", debug: bool = False
+    ) -> dict:
         server_params = StdioServerParameters(
             command="uv",
-            args=[
-                "run",
-                "mcp-groceries-server",
-                "--vendor",
-                os.environ["MCP_VENDOR"]
-            ],
+            args=["run", "mcp-groceries-server", "--vendor", os.environ["MCP_VENDOR"]],
             transport="stdio",
             env=dict(
                 VENDOR_API_KEY=os.environ.get("VENDOR_API_KEY"),
@@ -46,21 +44,27 @@ class GroceriesAgent:
                 CART_ID=os.environ.get("CART_ID"),
             ),
         )
-        async with stdio_client(server_params) as (read, write):
-            async with ClientSession(read, write) as session:
-                await session.initialize()
 
-                # Get tools
-                tools = await load_mcp_tools(session)
-                prompts_result = await session.get_prompt(
-                    "start_shopping",
-                    arguments={
-                        "shopping_list": shopping_list,
-                        "preferences": preferences,
-                    },
-                )
-                agent = create_react_agent(self._model, tools, debug=debug)
-                prompts = [msg.content.text for msg in prompts_result.messages]
-                return await agent.ainvoke(
-                    {"messages": prompts}, {"recursion_limit": 50}
-                )
+        with self.console.status("[bold green] Start shopping") as status:
+            async with stdio_client(server_params) as (read, write):
+                async with ClientSession(read, write) as session:
+                    await session.initialize()
+
+                    # Get tools
+                    tools = await load_mcp_tools(session)
+
+                    prompts_result = await session.get_prompt(
+                        "start_shopping",
+                        arguments={
+                            "shopping_list": shopping_list,
+                            "preferences": preferences,
+                        },
+                    )
+                    agent = create_react_agent(self._model, tools, debug=debug)
+                    prompts = [msg.content.text for msg in prompts_result.messages]
+                    status.update(status="[bold green] Shopping for groceries...")
+                    result = await agent.ainvoke(
+                        {"messages": prompts}, {"recursion_limit": 50}
+                    )
+                    status.update(status="[bold green] Shopping completed!")
+                    return result
