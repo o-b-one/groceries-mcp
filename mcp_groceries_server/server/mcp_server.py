536a76596dce031fa24f196dc0b4f95481ab6df7
Orel Balilti <orelba93@gmail.com>
2025-12-06 18:55:06 +0200

    build: add compose file for mcp

--- mcp_groceries_server/server/mcp_server.py
+++ mcp_groceries_server/server/mcp_server.py
@@ -3,11 +3,4 @@ from contextlib import asynccontextmanager
 from mcp.server.fastmcp import FastMCP
 from mcp_groceries_server.server.providers.shufersal._service import close_browser
 
-@asynccontextmanager
-async def lifespan(app: FastMCP):
-    # Startup
-    yield
-    # Shutdown
-    # await close_browser()
-
-server = FastMCP("Groceries", lifespan=lifespan, host="0.0.0.0")
+server = FastMCP("Groceries", host="0.0.0.0")
